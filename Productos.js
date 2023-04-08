b7b0426829a897b38fe4435700aa71405fac029f
Almaylastoscas <alitocassan@gmail.com>
2023-04-07 21:02:03 -0300

    Primera entrega del curso class products

--- Productos.js
+++ Productos.js
@@ -1,1 +1,48 @@
-console.log("Arrancamos el curso de node.js");
+class ProductManager {
+  constructor() {
+    this.products = [];
+  }
+  addProducts({ title, description, price, thumbnail, Stock }) {
+    let id = 0;
+    if (this.products.length === 0) {
+      id = 1;
+    } else {
+      let lastProducts = this.products[this.products.length - 1];
+      id = lastProducts.id + 1;
+    }
+    let producto = { title, description, price, thumbnail, id, Stock };
+    this.products.push(producto);
+  }
+  getProducts() {
+    console.log(this.products);
+    return this.products;
+  }
+  getProductById = (id) => {
+    let resultado = this.products.find((e) => e.id === id);
+    if (resultado) {
+      console.log(resultado);
+      return resultado;
+    } else {
+      console.log("NOT FUND");
+    }
+  };
+}
+
+// let newProduct = new ProductManager();
+// newProduct.addProducts({
+//   title: "zapatilla nike",
+//   description: "zapatilla deportive",
+//   price: 535,
+//   thumbnail: "./fotos.js",
+//   Stock: "10",
+// });
+// newProduct.addProducts({
+//   title: "zapatilla adidas",
+//   description: "zapatilla deportive",
+//   price: 334,
+//   thumbnail: "./fotos2.js",
+//   Stock: "5",
+// });
+// newProduct.getProducts();
+// newProduct.getProductById(1);
+// newProduct.getProductById(8);
---
+++ README.md
@@ -0,0 +1,16 @@
+# Primera entrega del curso de node.js
+
+## En esta entrega se realizo una clase producto que se reutilizara para el proyecto final .
+
+### La clase tiene tres metodos y un constructor donde tenemos un array vacio donde se pushean los items .
+
+** Los metodos de la clase son : **
+
+1. addProducts
+2. getProducts
+3. getProductsById
+4.
+
+** para correr el archivo debe correr el comando : **
+
+- node productos.js
